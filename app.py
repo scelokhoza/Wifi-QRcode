5652dd7fafca5b0d1e797e81d914a2b49b8f043d
sceloprince <sceloprince749@gmail.com>
2024-10-03 09:32:40 +0200

    documenting with docstrings and implementing the flask backend

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request, redirect, url_for
+from backend.wifi_qr_code import WifiQrCode
+import os
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        wifi_name = request.form['wifi_name']
+        password = request.form['password']
+        auth_type = request.form['auth_type']
+        hidden = 'hidden' in request.form
+
+        qr_code = WifiQrCode(wifi_name, password, auth_type, hidden)
+        qr_code.create_image()
+
+        return redirect(url_for('display_qr'))
+    return render_template('index.html')
+
+
+@app.route('/qr')
+def display_qr():
+    return render_template('qr.html')
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- backend/wifi_qr_code.py
+++ backend/wifi_qr_code.py
@@ -1,4 +1,3 @@
-import os
 import img2pdf
 from wifi_qrcode_generator import wifi_qrcode
 
@@ -6,12 +5,28 @@ from wifi_qrcode_generator import wifi_qrcode
 
 class WifiQrCode:
     def __init__(self, wifi_name: str, password: str, authentication_type=None, hidden: bool = False):
+        """
+        Constructor for WifiQrCode.
+
+        Args:
+        wifi_name (str): The name of the wifi network.
+        password (str): The password of the wifi network.
+        authentication_type (str, optional): The authentication type of the wifi network. Defaults to None.
+        hidden (bool, optional): Whether the wifi network is hidden. Defaults to False.
+
+        """
         self.ssid: str = wifi_name
         self.password: str = password
         self.authentication_type: str = "WPA" if authentication_type is None else authentication_type
         self.hidden: bool = hidden
 
     def generate_qr_code(self):
+        """
+        Generates a QR code for the given wifi credentials.
+
+        Returns:
+        wifi_qrcode object: The QR code for the given wifi credentials.
+        """
         return wifi_qrcode(
             ssid=self.ssid,
             hidden=self.hidden,
@@ -21,13 +36,24 @@ class WifiQrCode:
 
 
     def create_image(self) -> None:
-        self.generate_qr_code().make_image().save('wifi_qrcode.pdf')
+        """
+        Creates an image of the QR code and saves it to a file.
+
+        """
+        self.generate_qr_code().make_image().save('static/asserts/wifi_qrcode.png')
 
 
     def make_pdf_qr_code(self):
+        """
+        Converts the QR code image to a PDF and saves it to a file.
+
+        This is done by using the img2pdf library to convert the QR code image to a PDF.
+
+        The PDF is saved to a file named 'qr_code.pdf'.
+
+        """
         with open("qr_code.pdf","wb") as f:
             f.write(img2pdf.convert('wifi_qrcode.png'))
-            os.remove('wifi_qrcode.png')
 
 
 
