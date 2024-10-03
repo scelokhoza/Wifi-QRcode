3444a44eb7ce58a71f5476e961b17d4ad2989212
sceloprince <sceloprince749@gmail.com>
2024-10-03 09:41:51 +0200

    making the pdf qr code to be saved in the downloads folder by default

--- backend/wifi_qr_code.py
+++ backend/wifi_qr_code.py
@@ -1,3 +1,4 @@
+import os
 import img2pdf
 from wifi_qrcode_generator import wifi_qrcode
 
@@ -6,19 +7,28 @@ from wifi_qrcode_generator import wifi_qrcode
 class WifiQrCode:
     def __init__(self, wifi_name: str, password: str, authentication_type=None, hidden: bool = False):
         """
-        Constructor for WifiQrCode.
+        Initialize a WifiQrCode object.
 
         Args:
-        wifi_name (str): The name of the wifi network.
-        password (str): The password of the wifi network.
-        authentication_type (str, optional): The authentication type of the wifi network. Defaults to None.
-        hidden (bool, optional): Whether the wifi network is hidden. Defaults to False.
+            wifi_name (str): The ssid of the wifi network.
+            password (str): The password of the wifi network.
+            authentication_type (str, optional): The authentication type of the wifi network.
+            hidden (bool, optional): Whether the wifi network is hidden. Defaults to False.
 
+        Attributes:
+            ssid (str): The ssid of the wifi network.
+            password (str): The password of the wifi network.
+            authentication_type (str): The authentication type of the wifi network.
+            hidden (bool): Whether the wifi network is hidden.
+            downloads_folder (str): The path to the downloads folder.
+            pdf_file_path (str): The path to the pdf file.
         """
         self.ssid: str = wifi_name
         self.password: str = password
         self.authentication_type: str = "WPA" if authentication_type is None else authentication_type
         self.hidden: bool = hidden
+        self.downloads_folder = os.path.join(os.path.expanduser("~"), "Downloads")
+        self.pdf_file_path = os.path.join(self.downloads_folder, "qr_code.pdf")
 
     def generate_qr_code(self):
         """
@@ -45,14 +55,11 @@ class WifiQrCode:
 
     def make_pdf_qr_code(self):
         """
-        Converts the QR code image to a PDF and saves it to a file.
+        Converts the QR code image to a PDF and saves it to the Downloads folder.
 
         This is done by using the img2pdf library to convert the QR code image to a PDF.
-
-        The PDF is saved to a file named 'qr_code.pdf'.
-
         """
-        with open("qr_code.pdf","wb") as f:
+        with open(self.pdf_file_path, "wb") as f:
             f.write(img2pdf.convert('wifi_qrcode.png'))
 
 
